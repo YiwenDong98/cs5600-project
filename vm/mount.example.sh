081934f7545175c47d4fbc55dd894fc325cb79ab
YiwenDong <dong.yiw@husky.neu.edu>
2018-04-16 11:20:02 -0400

    Virtual machine run scripts

---
+++ vm/mount.example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo sshfs -o allow_other -o nonempty theguy@192.168.217.136:/home/lubuntu ./mount
---
+++ vm/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo /usr/local/bin/qemu-system-x86_64 -hda mount/lubuntu.img -boot c -m 512M
